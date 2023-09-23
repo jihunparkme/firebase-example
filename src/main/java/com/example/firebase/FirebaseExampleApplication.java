94677ed19b8def68180de73c72b891290a627286
Jihun Park <jihunpark.tech@gmail.com>
2023-09-23 15:56:31 +0900

    init: project

---
+++ src/main/java/com/example/firebase/FirebaseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirebaseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirebaseExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/firebase/FirebaseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
