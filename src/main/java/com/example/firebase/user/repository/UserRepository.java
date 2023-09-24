8df8e4bdd768b7f406d3321d66de73e8b507e3e7
Jihun Park <jihunpark.tech@gmail.com>
2023-09-24 19:16:21 +0900

    feat: add document and edit document api

--- src/main/java/com/example/firebase/user/repository/UserRepository.java
+++ src/main/java/com/example/firebase/user/repository/UserRepository.java
@@ -2,25 +2,27 @@ package com.example.firebase.user.repository;
 
 import com.example.firebase.user.domain.User;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Repository
 public class UserRepository {
 
     public static final String COLLECTION_NAME = "USERS";
+    private static final Firestore FIRE_STORE = FirestoreClient.getFirestore();
 
     public List<User> getAllUsers() throws ExecutionException, InterruptedException {
         List<User> list = new ArrayList<>();
-        Firestore db = FirestoreClient.getFirestore();
-        ApiFuture<QuerySnapshot> future = db.collection(COLLECTION_NAME).get();
+        ApiFuture<QuerySnapshot> future = FIRE_STORE.collection(COLLECTION_NAME).get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
         for (QueryDocumentSnapshot document : documents) {
             list.add(document.toObject(User.class));
@@ -28,4 +30,75 @@ public class UserRepository {
         return list;
     }
 
+    public void addUser(User user) {
+        Query query = FIRE_STORE.collection(COLLECTION_NAME).whereEqualTo("email", user.getEmail());
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+        DocumentReference document = null;
+        if (isNotExistEmail(querySnapshot)) {
+            document = FIRE_STORE.collection(COLLECTION_NAME).document();
+            user.setId(document.getId());
+            document.set(user);
+            log.info("새로운 문서가 추가되었습니다. document ID: {}", document.getId());
+        } else {
+            throw new RuntimeException("이미 가입된 이메일입니다.");
+        }
+    }
+
+    public void editUser(User user) {
+        Query query = FIRE_STORE.collection(COLLECTION_NAME).whereEqualTo("email", user.getEmail());
+        ApiFuture<QuerySnapshot> querySnapshot = query.get();
+
+        DocumentReference document = null;
+        if (isExistEmail(querySnapshot)) {
+            document = FIRE_STORE.collection(COLLECTION_NAME).document(user.getId());
+            document.update("name", user.getName());
+            document.update("update_dt", Timestamp.now());
+            log.info("문서가 수정되었습니다. document ID: {}", document.getId());
+        } else {
+            throw new RuntimeException("해당 이메일로 계정이 존재하지 않습니다.");
+        }
+    }
+
+    private boolean isNotExistEmail(ApiFuture<QuerySnapshot> querySnapshot) {
+        try {
+            return querySnapshot.get().isEmpty();
+        } catch (Exception e) {
+            log.error("문서 조회를 실패하였습니다.");
+            return false;
+        }
+    }
+
+    private boolean isExistEmail(ApiFuture<QuerySnapshot> querySnapshot) {
+        return !this.isNotExistEmail(querySnapshot);
+    }
+
+    public Optional<User> findUser(String email) {
+        Query query = FIRE_STORE.collection(COLLECTION_NAME).whereEqualTo("email", email);
+        ApiFuture<QuerySnapshot> future = query.get();
+        try {
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+            if (!documents.isEmpty()) {
+                return Optional.of(documents.get(0).toObject(User.class));
+            }
+            return Optional.empty();
+        } catch (Exception e) {
+            log.error("문서 조회를 실패하였습니다.");
+            return Optional.empty();
+        }
+    }
+
+    public long countAllUsers() {
+        CollectionReference collectionRef = FIRE_STORE.collection(COLLECTION_NAME);
+        ApiFuture<QuerySnapshot> query = collectionRef.get();
+        QuerySnapshot querySnapshot = null;
+        try {
+            querySnapshot = query.get();
+            long documentCount = querySnapshot.size();
+            return documentCount;
+        } catch (Exception e) {
+            log.error("문서 조회를 실패하였습니다.");
+            return 0L;
+        }
+    }
 }
--- src/test/java/com/example/firebase/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/firebase/user/repository/UserRepositoryTest.java
@@ -1,12 +1,15 @@
 package com.example.firebase.user.repository;
 
 import com.example.firebase.user.domain.User;
+import com.google.cloud.Timestamp;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 @SpringBootTest
@@ -16,9 +19,43 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    void method() throws Exception {
-        List<User> users = userRepository.getUsers();
+    void getAllUsers() throws Exception {
+        List<User> users = userRepository.getAllUsers();
         users.forEach(user -> System.out.println("user = " + user));
     }
 
+    @Test
+    void addUser() throws Exception {
+        User user = User.builder()
+                .name("bbb")
+                .email("bbb@gmail.com")
+                .create_dt(Timestamp.now())
+                .build();
+
+        userRepository.addUser(user);
+    }
+
+    @Test
+    void fail_addUser() throws Exception {
+        User user = User.builder()
+                .name("Choi")
+                .email("choi@gmail.com")
+                .create_dt(Timestamp.now())
+                .build();
+
+        assertThatThrownBy(() -> userRepository.addUser(user))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void editUser() throws Exception {
+        User user = User.builder()
+                .id("yXMhDPpulEWGqO3ERzEU")
+                .name("Aaron1")
+                .email("aaron@gmail.com")
+                .create_dt(Timestamp.now())
+                .build();
+
+        userRepository.editUser(user);
+    }
 }
